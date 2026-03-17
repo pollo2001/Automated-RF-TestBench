622864b84f287e125de7a0be128640358c81dc46
Genaro Salazar Ruiz <genaro_salazar@ymail.com>
2026-03-17 13:08:34 -0700

    Change main GUI script from main_gui.py to gui_main.py

--- src/run_sweep.bat
+++ src/run_sweep.bat
@@ -3,5 +3,5 @@ title "Launching AUTO RF Test Bench..."
 echo checking Python dependencies...
 pip install pyvisa pyvisa-py pyserial customtkinter --quiet
 echo Starting GUI...
-python main_gui.py
+python gui_main.py
 pause
